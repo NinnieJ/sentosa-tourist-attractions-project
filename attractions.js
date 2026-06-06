37d14418d77b3698cc754ecd63054be69630ff51
NinnieJ <ninniee.jurenyy@gmail.com>
2026-06-06 10:31:42 +0800

    .

--- attractions.js
+++ attractions.js
@@ -2,6 +2,7 @@ const attractions = [
   {
     name: "Universal Studios Singapore",
     image: 'images/uss.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 7:00 PM",
     TicketLink: "https://www.rwsentosa.com/en/attractions/universal-studios-singapore",
     categories: ["thrill", "family"],
     budgets: ["high"],
@@ -13,6 +14,7 @@ const attractions = [
   {
   name: "Adventure Cove Waterpark",
     image: 'images/adventure-cove.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 5:00 PM",
     TicketLink: "https://www.rwsentosa.com/en/attractions/adventure-cove-waterpark",
     categories: ["water", "thrill", "family"],
     budgets: ["medium", "high"],
@@ -24,6 +26,7 @@ const attractions = [
   {
     name: "Singapore Oceanarium",
     image: 'images/oceanarium.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 7:00 PM",
     TicketLink: "https://www.rwsentosa.com/en/play/singapore-oceanarium",
     categories: ["indoor", "family", "relax"],
     budgets: ["medium", "high"],
@@ -35,6 +38,7 @@ const attractions = [
   {
     name: "Skyline Luge Singapore",
     image: 'images/skylineluge.jpg',
+    operatingHours: "OPERATING HOURS : 11:00 AM - 9:30 PM",
     TicketLink: "https://sentosa.skylineluge.com/book-now-direct/?gad_source=1&gad_campaignid=21660893419&gbraid=0AAAAACT7CMb_C9Is2pHRjwWyu_SFVArRS&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtt44F4SMgIfpECRdgrs-YhxMac2kb-XJJBId7-eKfDboG8c_AYjvKsaAs-_EALw_wcB",
     categories: ["thrill", "family"],
     budgets: ["medium"],
@@ -46,6 +50,7 @@ const attractions = [
   {
     name: "Wings of Time",
     image: 'images/wingsoftime.jpg',
+    operatingHours: "OPERATING HOURS : 07:40 PM and 8:40 PM",
     TicketLink: "https://wings-of-time.singapore-tickets.com/?ci=1&cm=17463527444_141265279887_c_g_wings%20of%20time%20ticket%20purchase_e_&gad_source=1&gad_campaignid=17463527444&gbraid=0AAAAACRC4b3j8gzKHErk5rBfDkIvEmhNm&gclid=Cj0KCQjw_b_QBhCSARIsAP6hR4c5RLDqfPT0hGxoIAXLc6l4hBEcIMKLvJy3BxFjr8dSVUcVUYl-yoUaAkjFEALw_wcB",
     categories: ["family", "relax"],
     budgets: ["medium"],
@@ -57,6 +62,7 @@ const attractions = [
   {
     name: "Sentosa Sensoryscape",
     image: 'images/sensoryscape.jpg',
+    operatingHours: "OPERATING HOURS : 24 HOURS",
     categories: ["nature", "relax", "family"],
     budgets: ["low"],
     times: ["short", "half-day"],
@@ -67,6 +73,7 @@ const attractions = [
   {
     name: "Siloso Beach",
     image: 'images/silosobeach.jpg',
+    operatingHours: "OPERATING HOURS : 24 HOURS",
     categories: ["water", "nature", "relax"],
     budgets: ["low"],
     times: ["short", "half-day"],
@@ -77,6 +84,7 @@ const attractions = [
   {
     name: "Palawan Beach",
     image: 'images/palawanbeach.jpg',
+    operatingHours: "OPERATING HOURS : 24 HOURS",
     categories: ["water", "nature", "family", "relax"],
     budgets: ["low"],
     times: ["short", "half-day"],
@@ -87,6 +95,7 @@ const attractions = [
   {
     name: "Tanjong Beach",
     image: 'images/tanjongbeach.jpg',
+    operatingHours: "OPERATING HOURS : 24 HOURS",
     categories: ["nature", "relax"],
     budgets: ["low"],
     times: ["short", "half-day"],
@@ -97,6 +106,7 @@ const attractions = [
   {
     name: "Madame Tussauds Singapore",
     image: 'images/madametussauds.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 6:00 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/madame-tussauds-singapore/?gclsrc=aw.ds&gad_source=1&gad_campaignid=23336964379&gbraid=0AAAAADlI5PECAn7nj5FX9FOrYtf53c_jo&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtva3XPQX4e_3S21BRtgFUZrALGfTdIiIXghaIKDv9iKxRrhDwiGukUaAhVzEALw_wcB",
     categories: ["indoor", "family"],
     budgets: ["medium"],
@@ -109,6 +119,7 @@ const attractions = [
   {
     name: "SkyHelix Sentosa",
     image: 'images/skyhelix.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 9:30 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/skyhelix-sentosa/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20684872723&gbraid=0AAAAADlI5PF5Fifg5UavA1fjqZf-tUDuj&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtuCYCAqaMwQZ8ESq3i_R5LnnKWpgjOxVY47dqQ7Qx52lA39Tf9bLGcaAiTaEALw_wcB",
     categories: ["thrill", "nature", "relax"],
     budgets: ["medium"],
@@ -120,6 +131,7 @@ const attractions = [
   {
     name: "Mega Adventure Park",
     image: 'images/megaadventure.jpg',
+    operatingHours: "OPERATING HOURS : 11:00 AM - 7:00 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/mega-adventure-park/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20681302781&gbraid=0AAAAADlI5PEFQsQPRxLOqyjaWMod-0jPP&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtsj_zUYVEonJN_K7N_zRHOp8rJAiQRLjE3YqVPlcUjmSDwvlplz9XEaAmy2EALw_wcB",
     categories: ["thrill"],
     budgets: ["high"],
@@ -131,6 +143,7 @@ const attractions = [
   {
     name: "iFly Singapore",
     image: 'images/ifly.jpg',
+    operatingHours: "OPERATING HOURS : 9:00 AM - 10:00 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/altitudex/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20117792775&gbraid=0AAAAADlI5PF_Kmc0mYdy8EWDbmD9fU3u2&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtvZy11asuXSGRrtAuKs7Afh7iAgmV8scniNnh80lrzdFrsK3cP99eoaAom9EALw_wcB",
     categories: ["thrill", "indoor"],
     budgets: ["high"],
@@ -142,6 +155,7 @@ const attractions = [
   {
     name: "Fort Siloso",
     image: 'images/fortsiloso.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 6:00 PM",
     categories: ["nature", "family"],
     budgets: ["low"],
     times: ["short", "half-day"],
@@ -152,6 +166,7 @@ const attractions = [
   {
     name: "Singapore Cable Car",
     image: 'images/cablecar.jpg',
+    operatingHours: "OPERATING HOURS : 8:45 AM - 10:00 PM",
     TicketLink:"https://www.sentosa.com.sg/en/things-to-do/attractions/singapore-cable-car/?gclsrc=aw.ds&gad_source=1&gad_campaignid=8612993241&gbraid=0AAAAADlI5PH9mD80Qg3A1yg7CI6x7A3gc&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtvsoLxViWe3ec9yEckd5-7butFrzNQZlLbEHjk_Z65Jtkrbw41HDasaAmWMEALw_wcB",
     categories: ["nature", "relax", "family"],
     budgets: ["medium", "high"],
@@ -163,6 +178,7 @@ const attractions = [
   {
     name: "HydroDash",
     image: 'images/hydrodash.jpg',
+    operatingHours: "OPERATING HOURS : 10:00 AM - 6:00 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/hydrodash/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20684872723&gbraid=0AAAAADlI5PF5Fifg5UavA1fjqZf-tUDuj&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtvzVVQ5kkKehcLxVqXc47qAUETvj3xFUIH_7I0gXBMXJlC6NZGNx28aAuKTEALw_wcB",
     categories: ["water", "thrill", "family"],
     budgets: ["medium"],
@@ -174,6 +190,7 @@ const attractions = [
   {
     name: "Sentosa 4D AdventureLand",
     Images: 'images/4dadventureland.jpg',
+    operatingHours: "OPERATING HOURS : 12:00 PM - 7:00 PM",
     TicketLink: "https://www.sentosa.com.sg/en/things-to-do/attractions/sentosa-4d-adventureland/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20681302781&gbraid=0AAAAADlI5PEFQsQPRxLOqyjaWMod-0jPP&gclid=Cj0KCQjwoMXQBhDcARIsAH-eEtsZ8H3OjnIdiVvIR_xuMVNJ_lhfOlfMkJyVAuqjY8tM9rC6hjaPUTYaAnfaEALw_wcB",
     categories: ["indoor", "family"],
     budgets: ["medium"],
@@ -279,7 +296,7 @@ function createCard(attraction, score, reasons) {
       <i class="${attraction.icon}"></i>
       ${attraction.name}
     </h3>
-    <a href='"${attraction.ticketLink}" target="_blank">'>
+    <a href='"${attraction.TicketLink}" target="_blank">'>
     
     </a>
 
@@ -292,15 +309,29 @@ function createCard(attraction, score, reasons) {
 
   <p class="reason">${attraction.description}</p>
 
-  <a href="${attraction.TicketLink}" 
-     target="_blank" 
-     class="ticket-btn">
-     Buy Tickets
-  </a>
+  ${attraction.TicketLink ? `
+<a href="${attraction.TicketLink}" 
+   target="_blank" 
+   class="ticket-btn">
+   Buy Tickets
+   <i class="fa-solid fa-ticket"></i>
+</a>
+` : `
+<span class="free-badge">
+   <i class="fa-solid fa-gift"></i> FREE
+</span>
+`}
+
 
   <div class="meta">
     ${tags.map((tag) => `<span class="tag">${tag}</span>`).join("")}
   </div>
+
+  <p class="hours">
+  <i class="fa-solid fa-clock"></i>
+  ${attraction.operatingHours}
+</p>
+
 `;
   return card;
 }
